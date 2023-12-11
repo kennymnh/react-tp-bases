d97f6456dfa1fde408deaa802122e1bef8100c69
kennymnh <93839333+kennymnh@users.noreply.github.com>
2023-12-11 10:57:33 +0100

    Update Contact.jsx

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -2,6 +2,16 @@ import Footer from "../partial/Footer";
 import Header from "../partial/Header";
 
 const Contact = () => {
+
+    const handleContactSubmit = (event) => {
+        event.preventDefault();
+
+        const email = event.target.email.value;
+        const message = event.target.message.value;
+
+        console.log(email, message);
+    }
+
     return (
         <>
             <div className="App">
@@ -9,7 +19,23 @@ const Contact = () => {
 
                 <div className="section">
                     <h1>Contact</h1>
-                    <p>Ici se trouve le formulaire.</p>
+
+                    <form onSubmit={handleContactSubmit}>
+                        <div>
+                            <label>
+                                E-mail
+                                <input type="text" name="email" />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                Message
+                                <input type="text" name="message" />
+                            </label>
+                        </div>
+
+                        <input type="submit" />
+                    </form>
                 </div>
 
                 <Footer />
